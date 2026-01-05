2340893b817dc6cc71feabace6387b808d453e4b
prashantmore45 <prashantmorepm05@gmail.com>
2026-01-06 02:15:26 +0530

    "Lesson 02: Basic TypeScript types"

---
+++ lesson-02-basic-types/dist/js/main.js
@@ -0,0 +1,15 @@
+"use strict";
+let myName;
+let meaningOfLife;
+let isLoading;
+let album;
+myName = 'Prashant';
+meaningOfLife = 42;
+isLoading = true;
+album = 5150;
+const sum = (a, b) => {
+    return a + b;
+};
+let postId;
+let isActive;
+let re = /\w+/g;
