1e64e13aaffb8e6dfc5e53d8cb86d5796b2927c8
prashantmore45 <prashantmorepm05@gmail.com>
2026-01-29 22:16:14 +0530

    Lesson 07: Index signatures and keyof in TypeScript

---
+++ lesson-07-index-signatures-keyof/dist/js/main.js
@@ -0,0 +1,45 @@
+"use strict";
+//Index Signatures
+const todaysTransactions = {
+    Pizza: -10,
+    Books: -5,
+    Job: 50
+};
+console.log(todaysTransactions.Pizza);
+console.log(todaysTransactions['Pizza']);
+let prop = 'Pizza';
+console.log(todaysTransactions[prop]);
+const todaysNet = (transactions) => {
+    let total = 0;
+    for (const transaction in transactions) {
+        total += transactions[transaction]; //Index Signature
+    }
+    return total;
+};
+console.log(todaysNet(todaysTransactions));
+//todaysTransactions.Pizza = 40 //Error: Cannot assign to 'Pizza' because it is a read-only property.
+console.log(todaysTransactions['Dave']); //undefined
+const student = {
+    name: "Doug",
+    GPA: 3.5,
+    classes: [100, 200]
+};
+// console.log(student.test) //undefined
+for (const key in student) {
+    console.log(`${key}: ${student[key]}`); //Type Assertion
+}
+Object.keys(student).map(key => {
+    console.log(student[key]); //Type Assertion
+});
+const logStudentKey = (student, key) => {
+    console.log(`Student ${key}: ${student[key]}`);
+};
+logStudentKey(student, 'name');
+const monthlyIncomes = {
+    salary: 500,
+    bonus: 100,
+    sidehustle: 250
+};
+for (const revenue in monthlyIncomes) {
+    console.log(monthlyIncomes[revenue]); //Type Assertion
+}
